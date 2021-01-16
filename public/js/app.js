3d1a9ee86b395abe02558c5f3632e354f6f00aed
major-winter <tuanhoangchu@outlook.com>
2021-01-17 00:12:55 +0700

    add spinner

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,20 @@ const hamburger = document.querySelector('.hamburger')
 const mobileNavbar = document.querySelector('.mobile-navbar')
 const html = document.querySelector('html')
 
+const opening = document.querySelector('.opening')
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  opening.classList.add('opening')
+})
+
+window.onload = () => {
+  setTimeout(() => {
+    opening.classList.add('closing')
+
+  }, 1000)
+}
+
 $('.slider').slick({
   autoplay: true,
   autoplaySpeed: 1000,
